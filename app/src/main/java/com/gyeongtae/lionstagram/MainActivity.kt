da5e40af1dbf74a5ec6b82e22accc8fdadd8df07
Gyeongtae Kim <plodio.dev@gmail.com>
2021-02-01 18:38:44 +0900

    메인화면 및 탭뷰 구현

--- app/src/main/java/com/gyeongtae/lionstagram/MainActivity.kt
+++ app/src/main/java/com/gyeongtae/lionstagram/MainActivity.kt
@@ -2,10 +2,57 @@ package com.gyeongtae.lionstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+import com.gyeongtae.lionstagram.databinding.ActivityLoginBinding
+import com.gyeongtae.lionstagram.databinding.ActivityMainBinding
+import com.gyeongtae.lionstagram.navigation.AlarmFragment
+import com.gyeongtae.lionstagram.navigation.DetailViewFragment
+import com.gyeongtae.lionstagram.navigation.GridFragment
+import com.gyeongtae.lionstagram.navigation.UserFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.bottomNavigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
     }
 }
