7c99bfbb2dacc8708014f353266c7752012e0be2
Gyeongtae Kim <plodio.dev@gmail.com>
2021-02-06 19:24:35 +0900

    UserFragment에서 imageview 문제 해결 필요

--- app/src/main/java/com/gyeongtae/lionstagram/navigation/UserFragment.kt
+++ app/src/main/java/com/gyeongtae/lionstagram/navigation/UserFragment.kt
@@ -4,12 +4,77 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.gyeongtae.lionstagram.R
+import com.gyeongtae.lionstagram.databinding.FragmentDetailBinding
+import com.gyeongtae.lionstagram.databinding.FragmentUserBinding
+import com.gyeongtae.lionstagram.navigation.model.ContentDTO
 
-class UserFragment : Fragment(){
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
-        return view
+class UserFragment : Fragment() {
+    // 뷰가 사라질 때, 즉 메모리에서 날라갈 때 같이 날리기 위해 따로 빼두기
+    private var fragmentUserBinding: FragmentUserBinding? = null
+    var fragmentView: View? = null
+    var firestore: FirebaseFirestore? = null
+    var uid: String? = null
+    var auth: FirebaseAuth? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentUserBinding.inflate(inflater, container, false)
+        fragmentUserBinding = binding
+
+        firestore = FirebaseFirestore.getInstance()
+        return fragmentUserBinding!!.root
+    }
+
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var contentDTOs :ArrayList<ContentDTO> = arrayListOf()
+        init {
+            firestore?.collection("images")?.whereEqualTo("uid",uid)?.addSnapshotListener { qurrySnapshot, firebaseFirestoreException ->
+                if (qurrySnapshot == null) return@addSnapshotListener
+
+                for (snapshot in qurrySnapshot.documents){
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                fragmentUserBinding?.accountTvPostCount?.text = contentDTOs.size.toString()
+                notifyDataSetChanged()
+            }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var width = resources.displayMetrics.widthPixels / 3
+            var imageview = ImageView(parent.context)
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(imageview: ImageView) : RecyclerView.ViewHolder(imageview) {
+            
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUri).apply(
+                RequestOptions().centerCrop()).into(imageview)
+        }
+
+        override fun getItemCount(): Int {
+            return contentDTOs.size
+        }
+
+    }
+    override fun onDestroyView() {
+        fragmentUserBinding = null
+        super.onDestroyView()
     }
 }
